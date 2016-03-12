3dabb28cfa1a511023581b867fca108badcf6734
HDH1 <HDH@qq.com>
2016-03-12 09:42:17 +0800

    添加了dog类

---
+++ xxshueiying/xxshueiying/dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  xxshueiying
+//
+//  Created by 洪东恒 on 16/3/12.
+//  Copyright © 2016年 洪东恒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
---
+++ xxshueiying/xxshueiying/dog.m
@@ -0,0 +1,13 @@
+//
+//  dog.m
+//  xxshueiying
+//
+//  Created by 洪东恒 on 16/3/12.
+//  Copyright © 2016年 洪东恒. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
